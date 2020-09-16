827f28d8dc88c1d94321e1cfbdd2280718ba5d6a
Conor Driscoll <conor.driscoll@teamcolab.com>
2020-09-16 09:46:17 -0400

    try

---
+++ wp-content/themes/chimbo/functions.php
@@ -0,0 +1,7 @@
+<?php 
+
+/* enqueue scripts and style from parent theme */        
+function twentyChild_styles() {
+	wp_enqueue_style( 'parent', get_template_directory_uri() . '/style.css' );
+}
+add_action( 'wp_enqueue_scripts', 'twentyChild_styles');
